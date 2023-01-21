6f34ee42ea7d1520321d96182c64da5f037bf0e4
Hinterberger-Thomas <thomas.hinterberger.eu@gmail.com>
2023-01-21 21:42:07 +0100

    can now upload opt pics

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,11 @@ import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs()]
+  integrations: [tailwind(), solidJs(), image( {
+    serviceEntryPoint: "@astrojs/image/sharp"
+  })]
 });
